671a75471e1f852c5fa495458c37924ba2bd185a
dropjrj <happy920121@gmail.com>
2022-03-01 13:38:12 +0800

    b911

---
+++ ZeroJudge/b911.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int stick;
+    while (cin >> stick) {
+        cout << (int)log2(stick) + 1 << endl;
+    }
+
+    return 0;
+}
+
